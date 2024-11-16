fc8481b64cd15994a88e483af18267c605805a7d
EdiChiu <edisonnchiu@gmail.com>
2024-11-15 23:07:23 -0600

    Initial Commit

---
+++ Spark/SparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by Edison Chiu on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

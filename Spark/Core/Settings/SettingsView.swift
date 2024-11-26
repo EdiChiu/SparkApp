4da8f706015c7d46709815b6961dbec7b05aef2b
Diego Lagunas <dlagu8@illinois.edu>
2024-11-25 18:24:58 -0600

    Organized my files under a folder called Core.

--- Spark/Core/Settings/SettingsView.swift
+++ Spark/Core/Settings/SettingsView.swift
@@ -5,38 +5,6 @@
 //  Created by Diego Lagunas on 11/19/24.
 //
 import SwiftUI
-@MainActor
-final class SettingsViewModel: ObservableObject {
-    
-    func signOut() throws {
-        try AuthenticationManager.shared.signOut()
-    }
-    
-    func deleteAccount() async throws {
-        try await AuthenticationManager.shared.delete()
-    }
-    
-    func resetPassword() async throws {
-        let authUser = try AuthenticationManager.shared.getAuthenticatedUser()
-        
-        guard let email = authUser.email else {
-            throw URLError(.fileDoesNotExist)
-        }
-        
-        try await AuthenticationManager.shared.resetPassword(email: email)
-    }
-    
-    func updateEmail () async throws {
-        let email = "hello123@gmail.com"
-        try await AuthenticationManager.shared.updateEmail(email: email)
-    }
-    
-    func updatePassword() async throws {
-        let password = "hello123"
-        try await AuthenticationManager.shared.updatePassword(password: password)
-    }
-    
-}
 struct SettingsView: View {
     
     @StateObject private var viewModel = SettingsViewModel()
